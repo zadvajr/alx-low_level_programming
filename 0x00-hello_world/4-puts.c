dfd74e2c10d4aaf150f4d7e73f2256bb97e76bd2
zadvajr <danielzadva@gmail.com>
2023-06-15 14:25:42 -0700

    4. puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,11 @@
 /*main function goes here*/
 int main(void)
 {
-	/*prints to standard output*/
-	puts("Programming is like building a multilingual puzzle \n");
+	/**
+	 * main - A program that prints a line with puts function
+	 *  Return: 0 (Success)
+	 */	
+	puts("\"Programming is like building a multilingual puzzle \n");
 
 	return (0);
 }
