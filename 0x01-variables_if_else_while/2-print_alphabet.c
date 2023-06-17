137150ff3a1532000e2c4e3b468e36b2840ca24b
zadvajr <danielzadva@gmail.com>
2023-06-17 16:31:28 -0700

    2. print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 97;
+	for(n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+
+	return (0);
+}
