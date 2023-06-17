0294d2573b96140b7f5b9ca4105b0a8b6ce6cb19
zadvajr <danielzadva@gmail.com>
2023-06-17 16:53:00 -0700

    3. print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,20 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 	int m;
-
-        n = 97;
-        for (n = 97; n <= 122; n++)
+	
+	for (n = 97; n <= 122; n++)
         {
-                putchar(n);
+		putchar(n);
         }
 
 	for (m = 65; m <= 90; m++)
 	{
 		putchar(m);
 	}
-        putchar(10);
-        return (0);
+	
+	putchar(10);
+	
+	return (0);
 }
