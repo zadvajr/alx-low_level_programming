a782882c4f8f8db783deddcd9d53c99f64eaa838
zadvajr <danielzadva@gmail.com>
2023-06-17 17:24:40 -0700

    5. print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry Poinnt for all codes
+ *
+ * Returns: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 48;
+	while (n <58)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
