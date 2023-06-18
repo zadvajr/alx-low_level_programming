f521386d46e52e33da6f2ec32939caff0debf472
zadvajr <danielzadva@gmail.com>
2023-06-17 17:37:41 -0700

    6. numberz

---
+++ 0x01-variables_if_else_while/6-numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int n;
+
+        n = 48;
+        while (n < 58)
+        {
+                putchar(n);
+                n++;
+        }
+
+        putchar(10);
+
+        return (0);
+}
