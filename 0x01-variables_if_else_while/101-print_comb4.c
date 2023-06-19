2521d739a84eba7a0df38b7f75b9739acc96055d
zadvajr <danielzadva@gmail.com>
2023-06-19 13:09:22 -0700

    101. print combo 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ * program to print all possible combination of 3 digits numbers
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			for (k = 1; k <= 9; k++)
+			{
+				if ((j != i) != k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (i != '7' && j != '8')
+					{
+						continue;
+					}
+
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
