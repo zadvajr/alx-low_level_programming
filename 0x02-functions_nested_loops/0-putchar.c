5944ef22925e8146101c17de637d22f4901c14c9
zadvajr <danielzadva@gmail.com>
2023-06-20 09:02:15 -0700

    0. putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ * c source code to pritn "_putchar"
+ */
+int main (void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+#### Functions, Loops and Nested loops
