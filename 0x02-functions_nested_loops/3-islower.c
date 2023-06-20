9046a25f0f67172c063b1f4515b2cc32c3ae3391
zadvajr <danielzadva@gmail.com>
2023-06-20 15:22:48 -0700

    3. _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 /**
  * _islower - function code that checks whether a character is lower or not
  *
+ * @c: parameter to be used to store the arguments
  * Return: Always 1 or 0 depending on input (Succes)
  *
  */
 int _islower(int c)
 {
-	if (c >=97)
+	if (c >= 97)
 	{
 		return (1);
 	}
