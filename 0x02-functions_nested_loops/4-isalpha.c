07d3a36c153336fe8253fbb92dc08f3c36fcbdb6
zadvajr <danielzadva@gmail.com>
2023-06-21 11:25:52 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - function accepts one parameter and returns 1 if it is an alphabet or otherwise 0
+ *
+ * Return: Always integer value
+ *
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
