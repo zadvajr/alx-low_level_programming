dc2ebd09e5d1c0975d41743f5c45c00f35c3a142
zadvajr <danielzadva@gmail.com>
2023-06-26 15:53:08 -0700

    4. print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reversal order
+ *
+ * @s: parameter to store the string
+ *
+ * Return: Always void
+ *
+ */
+void print_rev (char *s)
+{
+	int i;
+	int count;
+	int j;
+
+	i = 0;
+	count = 0;
+
+	while (s[i] != '\0')
+	{
+		count += 1;
+	}
+
+	j = count;
+
+	while (s[j] != '\0')
+	{
+		_putchar(s[j]);
+		j--;
+	}
+	_putchar('\n');
+}
