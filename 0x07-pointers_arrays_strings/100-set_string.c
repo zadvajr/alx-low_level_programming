5d5001205f0a77b7b79f71dd42326bb664d087bc
zadvajr <danielzadva@gmail.com>
2023-07-04 13:50:42 -0700

    100-set_string function

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * set_string - sets string through a double pointer
+ *
+ * @s: double pointer parameter
+ * @to: string to be set 
+ *
+ * Return: Always 0.
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+
+	return;
+}
