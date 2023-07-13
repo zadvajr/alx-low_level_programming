110b8e6a6b38f6d4fb7c5c1ada2d7ed99de44843
zadvajr <danielzadva@gmail.com>
2023-07-13 13:32:39 -0700

    0-malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: number of bytes to allocate
+ *
+ * Return: Always returns a pointer to the allocated memory
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(*ptr) * b);
+
+	if(ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ *
+ * @c: The character to be printed
+ *
+ * Return: On success Always return 1.
+ * On error, -1 is returned and errno is set appropriately
+ *
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
