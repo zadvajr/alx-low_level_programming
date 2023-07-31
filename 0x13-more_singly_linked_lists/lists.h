5da5d05e7c5f359576a91322c2eff2328248db4a
zadvajr <danielzadva@gmail.com>
2023-07-31 13:24:05 -0700

    0-print_listint

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -9,13 +12,13 @@
  * Description: singly linked list node structure
  *
  */
+
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 }listint_t;
 
-/* Functions prototypes */
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
 
