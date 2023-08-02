9b1c360a4d20302c65fb8240e12e582b8ca7538d
zadvajr <danielzadva@gmail.com>
2023-08-02 12:41:08 -0700

    5-sqrt_recursion

--- 0x08-recursion/5-main.c
+++ 0x08-recursion/5-main.c
@@ -13,7 +13,7 @@ int main(void)
 	printf("%d\n", r);
 	r = _sqrt_recursion(1024);
 	printf("%d\n", r);
-	r = _sqrt_reursion(16);
+	r = _sqrt_recursion(16);
 	printf("%d\n", r);
 	r = _sqrt_recursion(17);
 	printf("%d\n", r);
@@ -21,4 +21,5 @@ int main(void)
 	printf("%d\n", r);
 	r = _sqrt_recursion(-1);
 	printf("%d\n", r);
+	return (0);
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,39 @@
 #include "main.h"
 
 /**
+ * _sqrt - Finds the square root
+ * @n: Number to be passed
+ * @g: square root guess
+ *
+ * Return: Returns g, otherwise -1
+ */
+
+int _sqrt(int n, int g)
+{
+	if (g * g > n)
+	{
+		return (-1);
+	}
+	else if (g * g == n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (_sqrt(n, g+1));
+	}
+}
+
+/**
  * _sqrt_recursion - Computes sqrt of n
  * @n: Number to compute sqrt of
  *
  * Return: Returns the sqrt of n
  */
+
 int _sqrt_recursion(int n)
 {
-
+	if (n < 0)
+		return (-1);
+	return (_sqrt(n, 0));
 }
