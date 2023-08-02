0e6625182f445c5f0997562c0a77a6acd845d794
zadvajr <danielzadva@gmail.com>
2023-08-02 07:55:24 -0700

    4-pow_recursion

---
+++ 0x08-recursion/4-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: Always return 0.
+ */
+int main(void)
+{
+	int r = 0;
+
+	r = _pow_recursion(1, 10);
+	printf("%d\n", r);
+	r = _pow_recursion(1024, 0);
+	printf("%d\n", r);
+	r = _pow_recursion(2, 16);
+	printf("%d\n", r);
+	r = _pow_recursion(5, 2);
+	printf("%di", r);
+	r = _pow_recursion(5, -2);
+	printf("%d\n", r);
+	r = _pow_recursion(-5, 3);
+	printf("%d\n", r);
+	return (0);
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Computes x, to power of y
+ * @x: index x 
+ * @y: power y
+ *
+ * Return: Returns x to power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	int r = 0;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		r = x * _pow_recursion(x, (y - 1));
+		return (r);
+	}
+}
