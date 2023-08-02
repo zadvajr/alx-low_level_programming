076c3b974731b575ad15d333043e5d5280e04f6e
zadvajr <danielzadva@gmail.com>
2023-08-02 02:28:39 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: Always return 0. on success.
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Daniel Zadva Jnr is doing hard things with ALX SE");
+	printf("%d\n", n);
+	return (0);
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Prints the total number of characters in a string s
+ * @s: String to be passed
+ * 
+ * Return: On success returns 1.
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s)
+	{
+		count++;
+		count += _strlen_recursion(s + 1);
+	}
+	return (count);
+}
