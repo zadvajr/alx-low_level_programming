970d8abfd62b011147ff7489fcb6a0d95b8ebb80
zadvajr <danielzadva@gmail.com>
2023-08-02 12:24:04 -0700

    4-pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
 /**
- * _pow_recursion - Computes x, to power of y
+ * _pow_recursion - Computes x to power of y
  * @x: index x 
  * @y: power y
- *
  * Return: Returns x to power of y
  */
-
 int _pow_recursion(int x, int y)
 {
 	int r = 0;
