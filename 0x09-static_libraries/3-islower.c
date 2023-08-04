56bdf3fb0f27e32b9b9a2831b4386be39a59b088
zadvajr <danielzadva@gmail.com>
2023-08-04 13:45:53 -0700

    added something sha

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - function code that checks whether a character is lower or not
+ *
+ * @c: parameter to be used to store the arguments
+ * Return: Always 1 or 0 depending on input (Succes)
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 97)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
