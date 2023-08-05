dea0b99be2c640aafca371a7070324838240da1f
zadvajr <danielzadva@gmail.com>
2023-08-05 14:38:27 -0700

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Returns an integer value
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argc == 2)
+	{
+		printf("%d\n", atoi(argv[1]));
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (EXIT_SUCCESS);
+}
