4dd6507c94f45b1c1fd271b276d15526ff4d9e82
zadvajr <danielzadva@gmail.com>
2023-08-05 14:00:48 -0700

    0-whasmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - checks the coded
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Returns an integer
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Functions Prototypes */
+int _putchar(int c);
+#endif
