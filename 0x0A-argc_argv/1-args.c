0138e405d0524700016b25646680bb582e0d72f7
zadvajr <danielzadva@gmail.com>
2023-08-05 14:07:38 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Returns an integer value
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (EXIT_SUCCESS);
+}
